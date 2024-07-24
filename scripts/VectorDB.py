2c36955cddac77ce61b961c87d749bbc0a2a8371
TagoreZhao <tagorezhao@berkeley.edu>
2024-07-24 15:37:10 -0700

    finish query

--- scripts/VectorDB.py
+++ scripts/VectorDB.py
@@ -22,11 +22,13 @@ class VectorDB:
     def _load_or_create_vector_db(self):
         """Load an existing Chroma vector database or create a new one."""
         # Ensure the directory exists
+        logging.info("Loading or creating Chroma vector database...")
         self.chroma_db_dir.mkdir(parents=True, exist_ok=True)
 
         client = chromadb.PersistentClient(path=str(self.chroma_db_dir))
     
         collection = client.get_or_create_collection(self.collection_name)
+        logging.info(f"Database '{self.collection_name}' loaded or created at '{self.chroma_db_dir}'.")
 
         return collection
     
@@ -57,26 +59,44 @@ class VectorDB:
 
     def load_url(self):
         """Load and process data from URLs."""
+        logging.info("Loading data from URLs...")
         urls = self.raw_data.get_urls()
+        sources = self.show_sources()
         for url in urls:
+            logging.info(f"Processing URL: {url}")
             text = self.raw_data.crawl_and_extract(url)
+            if url in sources:
+                self.delete_source(url)
             self._process_and_add(text, [str(url)] * len(text))
+        logging.info("Finished loading data from URLs.")
     
     def load_pdf(self):
         """Load and process data from PDFs."""
+        logging.info("Loading data from PDFs...")
         pdfs = self.raw_data.get_pdf()
-        for pdf in pdfs:
+        sources = self.show_sources()
+        for pdf in pdfs:    
+            logging.info(f"Processing PDF: {pdf}")
             text = self.raw_data.extract_pdf_texts(pdf)
             source_name = Path(pdf).name.replace('.pdf', '')  # Remove the '.pdf' extension
+            if source_name in sources:
+                self.delete_source(source_name)
             self._process_and_add(text, [str(source_name)] * len(text))
+        logging.info("Finished loading data from PDFs.")
 
     def load_html(self):
         """Load and process data from HTML files."""
+        logging.info("Loading data from HTML files...")
         htmls = self.raw_data.get_html()
+        sources = self.show_sources()
         for html in htmls:
+            logging.info(f"Processing HTML: {html}")
             source_name = Path(html).name.replace('.html', '')  # Remove the '.pdf' extension
             text = self.raw_data.extract_html_text(html)
+            if source_name in sources:
+                self.delete_source(source_name)
             self._process_and_add(text, [str(source_name)] * len(text))
+        logging.info("Finished loading data from HTML files.")
 
     def peek(self):
         """Retrieve first 10 documents from the vector database."""
@@ -97,3 +117,40 @@ class VectorDB:
             logging.info(f"Deleted documents from source: {source}")
         except Exception as e:
             logging.error(f"Error deleting documents: {e}")
+
+    def show_sources(self):
+        """Show all sources in the vector database."""
+        try:
+            sources = self.vector_db.get(
+                include=["metadatas"]
+            )
+            logging.info(f"Retrieved sources from the vector database.")
+            return set([s["source"] for s in sources["metadatas"]])
+        except Exception as e:
+            logging.error(f"Error getting sources: {e}")
+
+    def query_sources(self, source):
+        """Query the vector database with a specific source."""
+        try:
+            response = self.vector_db.query(
+                where={"source": source}
+            )
+            logging.info(f"Query results: {response}")
+            return response["documents"]
+        except Exception as e:
+            logging.error(f"Error querying the vector database: {e}")
+            return []
+        
+    def query(self, query_text, k=5):
+        """Query the vector database with a text prompt."""
+        try:
+            response = self.vector_db.query(
+                query_embeddings=ollama.embeddings(prompt=query_text, model=self.model)['embedding'],
+                n_results=k
+            )
+            logging.info(f"Query results: {response}")
+            return response["documents"][0]
+        except Exception as e:
+            logging.error(f"Error querying the vector database: {e}")
+            return []
+        
